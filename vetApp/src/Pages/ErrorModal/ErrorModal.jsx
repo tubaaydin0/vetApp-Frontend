b1ee280b872633769d2b90272017be0d4d4f1823
tubaaydin0 <tuba.aydin94@gmail.com>
2024-03-19 23:25:46 +0300

    commit

--- vetApp/src/Pages/ErrorModal/ErrorModal.jsx
+++ vetApp/src/Pages/ErrorModal/ErrorModal.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ErrorModal.css'
-
+import errorPicture from "../../assets/errorPicture.png";
 function ErrorModal({ error}) {
   return (
     <div className='error-content'>
-      <img src="./src/assets/dikkat.png" alt="dikkat gorseli" width= "6%"/>
+      <img src={errorPicture} alt="dikkat gorseli" width= "6%"/>
       <p>{error}</p>
       
     </div>
--- vetApp/src/Pages/Homepage/Homepage.jsx
+++ vetApp/src/Pages/Homepage/Homepage.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import './Homepage.css'
-import { useNavigate } from 'react-router-dom'
-//import login from "../../assets/images/login.png";
+import homepage from "../../assets/vetApp.png";
 
 
 function Homepage() {
 
-    const navigate = useNavigate();
-
-
-    const handleStartButton = () => {
-        navigate(`/questions`)
-    }
     return (
         <div className='container'>
             <div className="homepage-space">
                 <div className="homepage-title">
                         <h1>VET VETERİNER KLİNİĞİ</h1>
                 </div>
-                <img src="./src/assets/vetApp.png" width="30%" alt="homepage-image" />
+                <img src= {homepage} width="30%" alt="homepage-image" />
                 
 
             </div>
